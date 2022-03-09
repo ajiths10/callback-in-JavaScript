a6172ac8d5533f8cde059e8a099c4959d1059644
Nuclear One <ajithsnair10@gmail.com>
2022-03-09 12:26:01 +0530

    bug fix

--- callback.js
+++ callback.js
@@ -2,14 +2,16 @@ const posts=[
     {title:'Post One', body:'This is Post One',CreatedAt :new Date().getTime()},
     {title:'Post Two', body:'This is Post Two',CreatedAt :new Date().getTime()}
 ];
-
+let intervalId =0;
 
 function getposts(){
-        setInterval(()=>{
+        clearInterval(intervalId);    
+        intervalId= setInterval(()=>{
             let output = ''; 
             posts.forEach((post)=>{
             output+=`<li> ${post.title} Last Updated = ${(new Date().getTime() - post.CreatedAt)/1000} seconds ago</li>`;
             });
+            //console.log( `Timer id=${intervalId}`)
         document.body.innerHTML=output;
         },1000)
         
